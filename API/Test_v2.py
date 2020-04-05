0a01905cb8a281af0fa32729cd3c9d70beb7223c
AngelValera <angelvalera.epal@gmail.com>
2020-04-05 23:59:26 +0200

    Corrijo test

--- API/Test_v2.py
+++ API/Test_v2.py
@@ -18,7 +18,7 @@ class Test_Predicciones(unittest.TestCase):
     def test1_ProbocarError(self):
         with pytest.raises(ValueError):
             assert prediccion.ObtenerPrediccion('Prueba')
-     '''        
+             
     def test2_ObtenerPredicciones24(self):
         respuesta = app.get('/servicio/v1/prediccion/24horas')
         assert (respuesta.status_code == 200)
@@ -29,7 +29,7 @@ class Test_Predicciones(unittest.TestCase):
 
     def test4_ObtenerPredicciones72(self):
         respuesta = app.get('/servicio/v1/prediccion/72horas')
-        assert (respuesta.status_code == 200)  '''
+        assert (respuesta.status_code == 200)  
 
 if __name__ == '__main__':
     pytest.main()
